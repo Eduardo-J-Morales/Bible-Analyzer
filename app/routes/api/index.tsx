78f7ad7e19df395c21f4aed329a178d3f23ac38e
Eduardo Gonzalez <eduardojosegonzalezmorales41@gmail.com>
2025-03-27 10:17:41 -0400

    update

--- app/routes/api/index.tsx
+++ app/routes/api/index.tsx
@@ -48,7 +48,7 @@ export async function action({ request }: ActionFunctionArgs) {
   export async function loader({ request }: LoaderFunctionArgs) {
     setTimeout(async () => {
       storagedData[0] = null
-    }, 8000)
+    }, 10000)
     
     return json(storagedData[0], {
       headers: {
